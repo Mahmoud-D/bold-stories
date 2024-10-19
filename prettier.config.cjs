a23f93259e33e65507de5dc52d1c34dbbf771594
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-19 10:47:07 +0300

    chore: update Prettier configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+// .prettierrc
+
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+
+  semi: false,
+
+  singleQuote: true,
+
+  printWidth: 80,
+
+  bracketSpacing: true,
+
+  tabWidth: 2,
+
+  trailingComma: 'none'
+}

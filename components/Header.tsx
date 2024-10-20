b14394569b4aa696056922ad5e1589ba243a5bf7
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-20 15:36:08 +0300

    feat: add theme toggle functionality and dynamic SVG color

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,17 +68,19 @@ const Header = () => {
   const drawerRef = useRef<HTMLUListElement>(null)
 
   const toggleMenu = () => setOpenDrawer((prev) => !prev)
-  const { theme, setTheme } = useTheme()
 
+  const { theme, setTheme } = useTheme()
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
 
   return (
     <header className="dark:bb bb fixed z-30 h-[60px] w-full backdrop-blur-[10px]">
-      <div className="bl flex h-full w-full max-w-7xl items-center justify-between">
+      <div className="flex items-center justify-between w-full h-full bl max-w-7xl">
         {/* Logo Section */}
-        <div className="flex items-center gap-2">
+        <div
+          className={` ${theme === 'dark' ? 'text-white' : 'text-black'} flex items-center gap-2`}
+        >
           <a href="/" className="br p-[22px]">
             <svg
               width="20"
@@ -104,10 +106,10 @@ const Header = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M6.91837 15.3958L2.59196 9.17898H1.83191V15.3958H0V0.701545H2.22168C3.5274 0.701545 4.93056 0.721039 6.09986 1.38364C7.32763 2.10471 8.02921 3.48839 8.02921 4.89155C8.02921 6.9963 6.62605 8.75024 4.50182 9.00359L9.14004 15.3958H6.91837ZM1.83191 7.6589H2.41656C4.28744 7.6589 6.31423 7.3081 6.31423 4.9695C6.31423 2.66987 4.13154 2.33858 2.31912 2.33858H1.83191V7.6589ZM14.8545 15.3958V0.701545H16.6864V15.3958H14.8545ZM24.1742 15.3958H22.2059L29.2217 0.0779297L36.2375 15.3958H34.2692L32.5932 11.7125H25.8502L24.1742 15.3958ZM26.6103 10.0365H31.8332L29.2217 4.09252L26.6103 10.0365ZM52.6511 11.5566V0.701545H54.483V15.9999L43.4526 4.44332V15.3958H41.6206V-3.05176e-05L52.6511 11.5566ZM74.8137 7.9902V8.57485C74.8137 12.6089 71.9684 15.7076 67.8563 15.7076C63.7443 15.7076 60.4312 12.1607 60.4312 8.08764C60.4312 3.85867 63.8222 0.389737 68.0512 0.389737C70.3313 0.389737 72.592 1.42263 74.0341 3.23504L72.7479 4.4628C71.6955 3.02066 69.9221 2.06574 68.1292 2.06574C64.8356 2.06574 62.2632 4.73564 62.2632 8.02917C62.2632 11.1083 64.7966 14.0316 67.9732 14.0316C70.2534 14.0316 72.7089 12.0633 72.7869 9.66619H68.9866V7.9902H74.8137ZM87.2516 13.7198V15.3958H80.9568V0.701545H82.7887V13.7198H87.2516ZM100.683 0.701545V2.37755H94.6225V6.4896H100.508V8.1656H94.6225V13.7198H100.683V15.3958H92.7906V0.701545H100.683Z"
-                fill="#fff"
+                fill="currentColor"
               ></path>
             </svg>
           </a>
@@ -118,13 +120,13 @@ const Header = () => {
           initial="closed"
           animate="open"
           variants={desktopMenuVariants}
-          className="hidden items-center sm:flex"
+          className="items-center hidden sm:flex"
         >
           {['Work', 'About', 'Stories', 'Contact'].map((item) => (
             <motion.li
               key={item}
               variants={descitemVariants}
-              className="br bl p-4"
+              className="p-4 br bl"
             >
               <a href="#" className="text-lg font-medium">
                 {item}
@@ -139,7 +141,7 @@ const Header = () => {
         </motion.ul>
 
         {/* Animated Burger Button for Mobile */}
-        <div className="my-auto block pr-2 sm:hidden">
+        <div className="block pr-2 my-auto sm:hidden">
           <AnimatedHamburgerButton active={openDrawer} toggle={toggleMenu} />
         </div>
       </div>
@@ -150,13 +152,13 @@ const Header = () => {
         initial="closed"
         animate={openDrawer ? 'open' : 'closed'}
         variants={menuVariants}
-        className="fixed right-0 top-[70px] h-[calc(100vh-70px)] w-full bg-white dark:bg-darkBackground sm:hidden"
+        className="dark:bg-darkBackground fixed right-0 top-[70px] h-[calc(100vh-70px)] w-full bg-white sm:hidden"
       >
         {['Work', 'About', 'Stories', 'Contact'].map((item) => (
           <motion.li
             key={item}
             variants={itemVariants}
-            className="w-full border-b border-white/10 py-7 text-center"
+            className="w-full text-center border-b border-white/10 py-7"
           >
             <a href="#" className="block text-3xl font-semibold">
               {item}
@@ -166,7 +168,7 @@ const Header = () => {
 
         <motion.li
           variants={itemVariants}
-          className="flex w-full cursor-pointer items-center justify-center py-6 text-center"
+          className="flex items-center justify-center w-full py-6 text-center cursor-pointer"
         >
           <button onClick={toggleTheme} aria-label="Toggle theme">
             {theme === 'dark' ? <BsSun /> : <BsMoon />}

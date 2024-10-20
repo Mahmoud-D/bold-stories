f8ee7d8fe58f625df37ae00f0c8b1f42379e7f52
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-20 15:39:24 +0300

    feat: add responsive Heading component with custom fonts and styles

---
+++ components/Heading.tsx
@@ -0,0 +1,14 @@
+const Heading = () => {
+  return (
+    <div className="mt-6 p-5 sm:ml-20 sm:mt-20">
+      <h1 className="font-Magnita text-left text-3xl sm:text-justify sm:text-7xl">
+        Stories
+      </h1>
+      <h4 className="font-Magnita ml-4 text-right tracking-widest text-[#f43333] sm:text-justify">
+        From Riangle Team
+      </h4>
+    </div>
+  )
+}
+
+export default Heading

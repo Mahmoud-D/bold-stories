4961c6dfb557485cfd70b20e56c514d84ef07ae1
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-20 19:10:09 +0300

    refactor: edit responsive Heading component with custom fonts and styles

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,10 @@
 const Heading = () => {
   return (
-    <div className="mt-6 p-5 sm:ml-20 sm:mt-20">
-      <h1 className="font-Magnita text-left text-3xl sm:text-justify sm:text-7xl">
+    <div className="p-5 mt-6 sm:mb-7 sm:ml-24 sm:mt-16">
+      <h1 className="text-left font-magnita text-7xl sm:text-justify sm:text-8xl">
         Stories
       </h1>
-      <h4 className="font-Magnita ml-4 text-right tracking-widest text-[#f43333] sm:text-justify">
+      <h4 className="font-magnita ml-4 text-right tracking-widest text-[#f43333] sm:text-justify">
         From Riangle Team
       </h4>
     </div>

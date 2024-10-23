4467d2f9569283a22f55a4d46c2243472c73f43a
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-23 12:04:34 +0300

    refactor: extract code into separate reusable components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,97 +1,24 @@
 'use client'
 import { useState, useRef } from 'react'
-import { motion } from 'framer-motion'
 
+import DesktopNav from './DesktopNav'
+import MobileNav from './MobileNav'
 import AnimatedHamburgerButton from './AnimatedHamburgerButton'
-import { useTheme } from 'next-themes'
-import { BsMoon, BsSun } from 'react-icons/bs'
-
-const menuVariants = {
-  open: {
-    opacity: 1,
-    x: 0,
-    duration: 2,
-    transition: {
-      staggerChildren: 0.2, // Staggering the children animation by 0.1s
-      stiffness: 100,
-      damping: 15
-    }
-  },
-  closed: {
-    opacity: 0,
-    // duration: 2,
-
-    x: '100%',
-    transition: { stiffness: 100, damping: 15 }
-  }
-}
-
-const itemVariants = {
-  open: {
-    opacity: 1,
-    y: 0,
-    x: 0,
-    transition: { duration: 0.7 }
-  },
-  closed: {
-    opacity: 0,
-    y: 50, // Start from 20px below
-    x: 50,
-    transition: { duration: 0.7 }
-  }
-}
-
-const desktopMenuVariants = {
-  open: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      staggerChildren: 0.2, // Each child animates with 0.2s delay
-      duration: 0.3,
-      ease: 'easeOut'
-    }
-  },
-  closed: {
-    opacity: 0,
-    y: -20, // Items start 20px above
-    transition: { duration: 0.3, ease: 'easeIn' }
-  }
-}
-
-const descitemVariants = {
-  open: { opacity: 1, y: 0 },
-  closed: { opacity: 0, y: -20 }
-}
-
-const themeSunIconVariants = {
-  initial: { opacity: 0, y: -10 }, // Start slightly below
-  animate: { opacity: 1, y: 0, transition: { stiffness: 400, duration: 0.05 } }, // Fade in
-  exit: { opacity: 0, y: 10, transition: { stiffness: 400, duration: 0.05 } } // Fade out and move up
-}
-const themeMoonIconVariants = {
-  initial: { opacity: 0, y: 10 }, // Start slightly below
-  animate: { opacity: 1, y: 0, transition: { stiffness: 400, duration: 0.05 } }, // Fade in
-  exit: { opacity: 0, y: -10, transition: { stiffness: 400, duration: 0.05 } } // Fade out and move up
-}
+import Icon from './Icon'
+import Logo from './Logo'
 
 const Header = () => {
   const [openDrawer, setOpenDrawer] = useState(false)
+
   const drawerRef = useRef<HTMLUListElement>(null)
 
   const toggleMenu = () => setOpenDrawer((prev) => !prev)
 
-  const { theme, setTheme } = useTheme()
-  const toggleTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark')
-  }
-
   return (
     <header className="dark:dark-bb light-bb fixed z-30 h-[60px] w-full backdrop-blur-[10px]">
-      <div className="flex items-center justify-between w-full h-full light-bl bl dark:dark-bl max-w-7xl">
+      <div className="light-bl bl dark:dark-bl flex h-full w-full max-w-7xl items-center justify-between">
         {/* Logo Section */}
-        <div
-          className={` ${theme === 'dark' ? 'text-white' : 'text-black'} flex items-center gap-2`}
-        >
+        <div className="flex items-center gap-2 text-black dark:text-white">
           <a href="/" className="light-br br dark:dark:br p-[22px]">
             <Icon />
           </a>
@@ -101,84 +28,17 @@ const Header = () => {
         </div>
 
         {/* Desktop Menu */}
-        <motion.ul
-          initial="closed"
-          animate="open"
-          variants={desktopMenuVariants}
-          className="items-center hidden sm:flex"
-        >
-          {navLinks.map((item) => (
-            <motion.li key={item} variants={itemVariants} className="br bl p-4">
-              <a href="#" className="text-sm font-medium uppercase">
-                {item}
-              </a>
-            </motion.li>
-          ))}
-          <motion.li variants={itemVariants} className="p-6">
-            {/* Theme Toggle with Fade-Out-Up Animation */}
-            <motion.button
-              onClick={toggleTheme}
-              aria-label="Toggle theme"
-              key={theme} // Re-render and trigger animation on theme change
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              variants={
-                theme === 'dark' ? themeSunIconVariants : themeMoonIconVariants
-              }
-            >
-              {theme === 'dark' ? <BsSun /> : <BsMoon />}
-            </motion.button>
-          </motion.li>
-        </motion.ul>
+        <DesktopNav />
 
         {/* Animated Burger Button for Mobile */}
-        <div className="block pr-2 my-auto sm:hidden">
+        <div className="my-auto block pr-2 sm:hidden">
           <AnimatedHamburgerButton active={openDrawer} toggle={toggleMenu} />
         </div>
       </div>
 
       {/* Mobile Drawer Menu */}
-      <motion.ul
-        ref={drawerRef}
-        initial="closed"
-        animate={openDrawer ? 'open' : 'closed'}
-        variants={menuVariants}
-        className="dark:bg-darkBackground fixed right-0 top-[70px] h-[calc(100vh-70px)] w-full bg-white sm:hidden"
-      >
-        {navLinks.map((item) => (
-          <motion.li
-            key={item}
-            variants={itemVariants}
-            className="w-full text-center uppercase bb py-7"
-          >
-            <a href="#" className="block text-5xl font-magnita">
-              {item}
-            </a>
-          </motion.li>
-        ))}
-
-        <motion.li
-          variants={itemVariants}
-          className="flex items-center justify-center w-full py-6 text-center cursor-pointer"
-        >
-          <motion.button
-            onClick={toggleTheme}
-            aria-label="Toggle theme"
-            key={theme}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={
-              theme === 'dark' ? themeSunIconVariants : themeMoonIconVariants
-            }
-          >
-            {theme === 'dark' ? <BsSun /> : <BsMoon />}
-          </motion.button>
-        </motion.li>
-      </motion.ul>
+      <MobileNav openDrawer={openDrawer} drawerRef={drawerRef} />
     </header>
   )
 }
-
 export default Header

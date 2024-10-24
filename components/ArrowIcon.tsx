6fb1496c8a7851eadee8def1c36be07b6b17fff0
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-24 22:49:25 +0300

    refacotr: extract variants into sepreate file

--- components/ArrowIcon.tsx
+++ components/ArrowIcon.tsx
@@ -1,47 +1,11 @@
-import React from 'react'
 import { motion } from 'framer-motion'
+import {
+  firstSmallArrowVariants,
+  mainArrowVariants,
+  secondSmallArrowVariants
+} from '@/animations/arrow-icon-variants'
 
 const ArrowIcon = () => {
-  // Main Arrow: Pushes the first small arrow
-  const mainArrowVariants = {
-    animate: {
-      x: [0, 10, 0], // Move forward and reset
-      transition: {
-        duration: 1.5,
-        ease: 'easeInOut',
-        repeat: Infinity
-      }
-    }
-  }
-
-  // First Small Arrow: Moved by the main arrow
-  const firstSmallArrowVariants = {
-    animate: {
-      x: [0, 8, 0], // Moves slightly later than the main arrow
-      opacity: [0.3, 1, 0.3], // Fade in when moving, fade out when idle
-      transition: {
-        duration: 1.5,
-        ease: 'easeInOut',
-        delay: 0.2, // Starts after the main arrow
-        repeat: Infinity
-      }
-    }
-  }
-
-  // Second Small Arrow: Moved by the first small arrow
-  const secondSmallArrowVariants = {
-    animate: {
-      x: [0, 6, 0], // Moves slightly later than the first small arrow
-      opacity: [0.3, 1, 0.3], // Same fading effect
-      transition: {
-        duration: 1.5,
-        ease: 'easeInOut',
-        delay: 0.4, // Starts after the first small arrow
-        repeat: Infinity
-      }
-    }
-  }
-
   return (
     <motion.em className="flex items-center justify-center">
       {/* Main Arrow */}

bfc1e740221302a38efa7ccd4869c713f39ab86c
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-24 22:48:45 +0300

    refacotr: extract variants into sepreate file

--- components/AnimatedHamburgerButton.tsx
+++ components/AnimatedHamburgerButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { VARIANTS } from '@/animations/hamburger-button-variants'
 import { MotionConfig, motion } from 'framer-motion'
 
 interface AnimatedHamburgerButtonProps {
@@ -21,60 +21,22 @@ const AnimatedHamburgerButton: React.FC<AnimatedHamburgerButtonProps> = ({
         initial={false}
         animate={active ? 'open' : 'closed'}
         onClick={toggle}
-        className="relative w-10 h-10 transition-colors rounded-full bg-white/0"
+        className="relative h-10 w-10 rounded-full bg-white/0 transition-colors"
       >
         <motion.span
           variants={VARIANTS.top}
-          className="absolute w-10 h-1 bg-black dark:bg-white"
+          className="absolute h-1 w-10 bg-black dark:bg-white"
           style={{ y: '-50%', left: '50%', x: '-50%', top: '35%' }}
         />
 
         <motion.span
           variants={VARIANTS.middle}
-          className="absolute w-10 h-1 bg-black dark:bg-white"
+          className="absolute h-1 w-10 bg-black dark:bg-white"
           style={{ left: '50%', x: '-50%', top: '50%', y: '-50%' }}
         />
-        {/* <motion.span
-          variants={VARIANTS.bottom}
-          className="absolute w-5 h-1 bg-white"
-          style={{ left: '50%', x: '-50%', top: '65%', y: '-50%' }}
-        /> */}
       </motion.button>
     </MotionConfig>
   )
-}
-
-const VARIANTS = {
-  top: {
-    open: {
-      rotate: ['0deg', '0deg', '45deg'],
-      top: ['35%', '50%', '50%']
-    },
-    closed: {
-      rotate: ['45deg', '0deg', '0deg'],
-      top: ['50%', '50%', '35%']
-    }
-  },
-  middle: {
-    open: {
-      rotate: ['0deg', '0deg', '-45deg']
-    },
-    closed: {
-      rotate: ['-45deg', '0deg', '0deg']
-    }
-  },
-  bottom: {
-    open: {
-      rotate: ['0deg', '0deg', '45deg'],
-      bottom: ['35%', '50%', '50%'],
-      left: '50%'
-    },
-    closed: {
-      rotate: ['45deg', '0deg', '0deg'],
-      bottom: ['50%', '50%', '35%'],
-      left: '50%'
-    }
-  }
 }
 
 export default AnimatedHamburgerButton
